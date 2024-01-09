5aeb83d8b23b67fa5c469ec258c18356089ed1bd
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-01-09 17:14:26 +0100

    Implemented button component

--- src/components/smallComponents/ButtonComponent.vue
+++ src/components/smallComponents/ButtonComponent.vue
@@ -19,10 +19,21 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    .small {
-        background-color: blue;
-    }
-    .big {
-        background-color: orange;
+    @use '../../styles/partials/variables' as *;
+    button {
+        padding: .5rem 1.5rem;
+        border: none;
+        border-radius: 25px;
+        background-color: $btn-bg-color;
+        color: $btn-text-color;
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: .7rem;
+        .small {
+            // background-color: blue;
+        }
+        .big {
+            // background-color: orange;
+        }
     }
 </style>
